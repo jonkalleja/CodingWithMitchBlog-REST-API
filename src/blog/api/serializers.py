12dbdd373aff82b7cf1d35dd535d9003325b3bf4
Jon Kalleja <jonkalleja@gmail.com>
2020-11-17 19:09:29 +0000

    added some packages & libraries

--- src/blog/api/serializers.py
+++ src/blog/api/serializers.py
@@ -9,7 +9,7 @@ IMAGE_SIZE_MAX_BYTES = 1024 * 1024 * 2 # 2MB
 MIN_TITLE_LENGTH = 5
 MIN_BODY_LENGTH = 50
 
-from blog.utils import is_image_aspect_ratio_valid, is_image_size_valid
+#from blog.utils import is_image_aspect_ratio_valid, is_image_size_valid
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
